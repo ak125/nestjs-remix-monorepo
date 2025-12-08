08d17fc8aad0c0629e01d3d06d33bdbde80c62be
auto pieces equipement <automecanik.seo@gmail.com>
2025-12-08 13:11:56 +0000

    fix(sitemap): add missing xmlns:image namespace for Google validation
    - Add xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" to urlset
    - Fixes Google Search Console sitemap validation error
    - Required because sitemap contains <image:image> tags

--- frontend/app/routes/sitemap-gamme-produits[.]xml.tsx
+++ frontend/app/routes/sitemap-gamme-produits[.]xml.tsx
@@ -33,7 +33,7 @@ function combineSitemaps(xmlContents: string[]): string {
   const allUrls = xmlContents.flatMap(extractUrls);
   
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${allUrls.join('\n')}
 </urlset>`;
 }
